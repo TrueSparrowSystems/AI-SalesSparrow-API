cf914ca4cd4a26aff6c537d7ea0a8faf8a1781d4
Kiran Kamalakar <kamalakarkiran07@gmail.com>
2023-08-02 12:02:43 +0530

    dynamo connection configuration changes

---
+++ src/main/java/com/salessparrow/api/repositories/SalesforceOrganizationRepository.java
@@ -0,0 +1,19 @@
+package com.salessparrow.api.repositories;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.salessparrow.api.domain.SalesforceOrganization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SalesforceOrganizationRepository {
+
+    @Autowired
+    private DynamoDBMapper dynamoDBMapper;
+
+    public SalesforceOrganization saveSalesforceOrganization(SalesforceOrganization sfo) {
+        dynamoDBMapper.save(sfo);
+        return sfo;
+    }
+}
