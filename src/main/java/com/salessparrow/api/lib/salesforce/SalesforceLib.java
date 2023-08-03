7c53d35f1851bedcfb4a1eadb4611d31cd526856
Raj Shah <rajshah0211@gmail.com>
2023-08-03 17:03:52 +0530

    Updated url

--- src/main/java/com/salessparrow/api/lib/salesforce/SalesforceLib.java
+++ src/main/java/com/salessparrow/api/lib/salesforce/SalesforceLib.java
@@ -1,8 +1,5 @@
 package com.salessparrow.api.lib.salesforce;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +7,8 @@ public class SalesforceLib {
   
   public String getAccountsUrl(String q) {
     if (q == "") {
-      return "/services/data/v58.0/query/?q=SELECT Id, Name FROM Account ORDER BY LastModifiedDate DESC LIMIT 2";
+      return "/services/data/v58.0/query/?q=SELECT Id, Name FROM Account ORDER BY LastModifiedDate DESC LIMIT 20";
     } 
-    return "/services/data/v58.0/query/?q=SELECT Id, Name FROM Account WHERE Name LIKE '%25"+q+"%25' ORDER BY LastModifiedDate DESC LIMIT 2";
+    return "/services/data/v58.0/query/?q=SELECT Id, Name FROM Account WHERE Name LIKE '%25"+q+"%25' ORDER BY LastModifiedDate DESC LIMIT 20";
   }
 }
