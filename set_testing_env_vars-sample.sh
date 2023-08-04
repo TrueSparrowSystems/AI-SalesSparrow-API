27fe7d4dce233f6d6b87f93bab651eb9a7380c51
Kiran Kamalakar <kamalakarkiran07@gmail.com>
2023-08-04 20:05:51 +0530

    updated git ignore and added test env sample variables

---
+++ set_testing_env_vars-sample.sh
@@ -0,0 +1,20 @@
+# Application
+export ENVIRONMENT='test'
+
+# Authentication
+export ENCRYPTION_KEY='1234567890'
+export API_COOKIE_SECRET='1234567890'
+
+# AWS
+export AWS_ACCESS_KEY_ID=''
+export AWS_SECRET_ACCESS_KEY=''
+export AWS_REGION=''
+export KMS_KEY_ID=''
+export CACHE_CLUSTER_ID=''
+
+# Database
+export DYNAMO_DB_URL=''
+
+# Salesforce
+export SALESFORCE_CLIENT_ID=''
+export SALESFORCE_CLIENT_SECRET=''
