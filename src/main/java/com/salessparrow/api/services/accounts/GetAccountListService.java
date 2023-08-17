f1b050abee475670f4bbf76e7e6119db81223fe0
Raj Shah <rajshah0211@gmail.com>
2023-08-17 14:08:45 +0530

    Updated RequestParam to use dto in get accounts

--- src/main/java/com/salessparrow/api/services/accounts/GetAccountListService.java
+++ src/main/java/com/salessparrow/api/services/accounts/GetAccountListService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.salessparrow.api.domain.User;
 import com.salessparrow.api.dto.formatter.GetAccountsFormatterDto;
+import com.salessparrow.api.dto.requestMapper.GetAccountsDto;
 import com.salessparrow.api.lib.crmActions.getAccounts.GetAccountsFactory;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,10 +15,10 @@ public class GetAccountListService {
   @Autowired
   private GetAccountsFactory getAccountsFactory;
   
-  public GetAccountsFormatterDto getAccounts(HttpServletRequest request, String q) {
+  public GetAccountsFormatterDto getAccounts(HttpServletRequest request, GetAccountsDto getAccountsDto) {
     User user = (User) request.getAttribute("user");
 
-    String formattedSearchString = formatSearchString(q);
+    String formattedSearchString = formatSearchString(getAccountsDto.getQ());
     return getAccountsFactory.getAccounts(user, formattedSearchString);
   }
 
