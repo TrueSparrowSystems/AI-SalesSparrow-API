49036245b8e65f44028e00ffaa8aa8502162f06b
Yagnik Thummar <yagnikthummar0204@gmail.com>
2023-08-17 16:14:42 +0530

    Added paramErrorIdentifier field.

--- src/main/java/com/salessparrow/api/lib/errorLib/ParamErrorConfig.java
+++ src/main/java/com/salessparrow/api/lib/errorLib/ParamErrorConfig.java
@@ -1,17 +1,23 @@
 package com.salessparrow.api.lib.errorLib;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ParamErrorConfig {
     private String parameter;
+    private String paramErrorIdentifier;
     private String message;
 
     public ParamErrorConfig() {
     }
 
-    public ParamErrorConfig(String parameter, String message) {
+    public ParamErrorConfig(String parameter, String paramErrorIdentifier, String message) {
         this.parameter = parameter;
+        this.paramErrorIdentifier = paramErrorIdentifier;
         this.message = message;
     }
 
@@ -19,6 +25,7 @@ public class ParamErrorConfig {
     public String toString() {
         return "{" +
                 "parameter='" + parameter + '\'' +
+                ", paramErrorIdentifier='" + paramErrorIdentifier + '\'' +
                 ", message='" + message + '\'' +
                 '}';
     }
