1f04f06a40a4b8c5f2414de662106b596be466b1
VRDighe <vaibhav@plgworks.com>
2023-08-18 18:11:53 +0530

    Updated compare errors method

--- src/test/java/com/salessparrow/api/helper/Common.java
+++ src/test/java/com/salessparrow/api/helper/Common.java
@@ -13,8 +13,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
 
 /**
  * Common is a helper class for the tests.
@@ -36,10 +36,10 @@ public class Common {
     Resource resource = resourceLoader.getResource(location);
     ObjectMapper objectMapper = new ObjectMapper();
     Map<String, FixtureData> fixtureMap = new HashMap<>();
-    fixtureMap = objectMapper.readValue(resource.getInputStream(), new TypeReference<HashMap<String, FixtureData>>() {});
+    fixtureMap = objectMapper.readValue(resource.getInputStream(), new TypeReference<HashMap<String, FixtureData>>() {
+    });
     return fixtureMap.get(key);
   }
-
 
   /**
    * Load the test scenario data from the given location.
@@ -51,7 +51,8 @@ public class Common {
   public List<Scenario> loadScenariosData(String location) throws IOException {
     Resource resource = resourceLoader.getResource(location);
     ObjectMapper objectMapper = new ObjectMapper();
-    return objectMapper.readValue(resource.getInputStream(), new TypeReference<List<Scenario>>() {});
+    return objectMapper.readValue(resource.getInputStream(), new TypeReference<List<Scenario>>() {
+    });
   }
 
   /**
@@ -60,17 +61,19 @@ public class Common {
    * @param testDataItem
    * @param contentAsString
    */
-  public void compareErrors(Scenario testDataItem, String contentAsString) {
-    Integer httpCode = JsonPath.read(contentAsString, "$.http_code");
+  public void compareErrors(Scenario testDataItem, String contentAsString) throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode resJsonNode = objectMapper.readTree(contentAsString);
+    Integer httpCode = resJsonNode.get("http_code").asInt();
     assertEquals(testDataItem.getOutput().get("http_code"), httpCode);
 
-    String code = JsonPath.read(contentAsString, "$.code");
+    String code = resJsonNode.get("code").asText();
     assertEquals(testDataItem.getOutput().get("code"), code);
 
     List<String> paramErrors = (List<String>) testDataItem.getOutput().get("param_errors");
     if (paramErrors != null) {
-      for ( int i = 0; i < paramErrors.size(); i++) {
-        String actualError = JsonPath.read(contentAsString, "$.param_errors[" + i + "].param_error_identifier");
+      for (int i = 0; i < paramErrors.size(); i++) {
+        String actualError = resJsonNode.get("param_errors").get(i).get("param_error_identifier").asText();
         assertEquals(paramErrors.contains(actualError), true);
       }
     }
