5fe8d04033ae1bfd77b96dd5894978b952e536af
Kiran Kamalakar <kamalakarkiran07@gmail.com>
2023-08-22 16:09:57 +0530

    removed logs added for debugging

--- src/main/java/com/salessparrow/api/SalesSparrowApi.java
+++ src/main/java/com/salessparrow/api/SalesSparrowApi.java
@@ -5,18 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import com.salessparrow.api.config.CoreConstants;
-
 @SpringBootApplication
 @EnableCaching // This enables caching
 @EnableAsync // This enables asynchronous processing in Spring
 public class SalesSparrowApi {
 
 	public static void main(String[] args) {
-
-		System.out.println("Env variables ---- : " + System.getenv());
-		System.out.println("Env variables ---- environment: " + CoreConstants.environment());
-		System.out.println("Env variables ---- aws region: " + CoreConstants.awsRegion());
 		SpringApplication.run(SalesSparrowApi.class, args);
 	}
 }
