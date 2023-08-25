4fdf01077523ceed674d2dc40d3d312671aa1528
Yagnik Thummar <yagnikthummar0204@gmail.com>
2023-08-25 16:47:51 +0530

    Updated Cookie value for testCases.

--- src/test/java/com/salessparrow/api/functional/controllers/crmOrganizationUserController/GetCrmOrganizationUserListTest.java
+++ src/test/java/com/salessparrow/api/functional/controllers/crmOrganizationUserController/GetCrmOrganizationUserListTest.java
@@ -82,7 +82,7 @@ public class GetCrmOrganizationUserListTest {
         // Read data from the scenario
         ObjectMapper objectMapper = new ObjectMapper();
 
-        String cookieValue = Constants.SALESFORCE_ACTIVE_USET_COOKIE_VALUE;
+        String cookieValue = Constants.SALESFORCE_ACTIVE_USER_COOKIE_VALUE;
         String q = (String) testScenario.getInput().get("q");
 
         // Prepare mock responses
