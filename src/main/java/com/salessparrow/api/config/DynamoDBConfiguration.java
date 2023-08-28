73c3ab1f22f7cf68bb5afd6c7fddf5fd7cdd50f9
amanbarbaria <amanbarbaria@gmail.com>
2023-08-28 17:08:41 +0530

    Added db logs

--- src/main/java/com/salessparrow/api/config/DynamoDBConfiguration.java
+++ src/main/java/com/salessparrow/api/config/DynamoDBConfiguration.java
@@ -8,6 +8,8 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 
 import java.time.Instant;
 
+import java.time.Instant;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
